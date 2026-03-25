e62a8c0e55ad15b507eb300b026bf50b4dca2d66
Lucasggh <lucassantosdimon@gmail.com>
2026-03-24 22:32:16 -0300

    arrumando gh-pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import './index.css'
 import Registrar from './pages/Registrar.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='min-h-screen'>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/Registrar" element={<Registrar />} />
         <Route path="/Login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </div>
   </StrictMode>,
 )
