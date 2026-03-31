38f8623bd82eeec721c9baaf6adba4af70602af7
LUCAS SANTOS DIMON <lucas_s_dimon@estudante.sesisenai.org.br>
2026-03-31 17:07:35 -0300

    colocando transactions

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3067/api",
+});
+
 function Home() {
-    return (  
-        <div className="bg-[#f8f5f2] min-h-screen">
-            oi
-        </div>
-    );
+  const location = useLocation();
+  const [balance, setBalance] = useState("R$0");
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      if (location.pathname === "/api/home") { // ajuste para sua rota real
+        try {
+          const response = await api.get("/balance", {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+              "Content-Type": "application/json",
+            },
+          });
+          setBalance(`R$${response.data.balance}`);
+          console.log(response.data.balance)
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    };
+
+    fetchBalance();
+  }, [location.pathname]);
+
+  return <div className="bg-[#f8f5f2] min-h-screen">{balance}</div>;
 }
 
 export default Home;
