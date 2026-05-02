4c69f6c0c9673130238c63d7fe77ce3dbd355e4e
Lucas <lucassantosdimon@gmail.com>
2026-05-01 23:00:03 -0300

    production

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import routes from "./src/routes/index.js";
 import cors from "cors"
 const app = express()
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: "https://ecarteira-taupe.vercel.app",
   credentials: true
 }));
 
