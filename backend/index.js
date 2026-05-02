e945198a6dc67c6d3c7668e29f745ffead6128ac
Lucas <lucassantosdimon@gmail.com>
2026-05-01 22:30:25 -0300

    fix

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,9 @@ app.use(express.json())
 app.use("/api", routes)
 
 if (process.env.NODE_ENV !== 'test') {
-    app.listen(process.env.SERVER_PORT, () => {
+    app.listen(process.env.PORT, () => {
         console.log("🚀 Servidor backend no ar!")
-        console.log(`Porta do backend: ${process.env.SERVER_PORT}`)
+        console.log(`Porta do backend: ${process.env.PORT}`)
     });
 }
 
